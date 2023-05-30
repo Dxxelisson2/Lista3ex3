86ba6024fc924c31f20e6c9543452aeceec6686b
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-05-30 17:01:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Sexo;
+
+            Console.WriteLine("digite o sexo M para Masculino F para feminino ");
+            Sexo = Console.ReadLine();
+
+
+            while (Sexo != "F" && Sexo != "M")
+            {
+                Console.WriteLine("Resposta invalida digite M/F para uma resposta valida");
+                Sexo= Console.ReadLine();
+            }
+
+                if (Sexo == "F")
+            
+                {
+                Console.WriteLine("Sexo Feminio selecionado");
+                 
+                 }
+
+
+                else 
+                {
+
+                Console.WriteLine("Sexo Masculino Selecionado");
+                
+                }
+
+
+            }
+        }
+    }
+
